bda041c7fad899a402de3fc29712762f4a0daa25
Kyle Calica <kycalica@gmail.com>
2015-09-18 00:24:08 -0700

    made some more changes

---
+++ src/main/java/infamous/team/Servlets/Blog.java
@@ -0,0 +1,22 @@
+package infamous.team.Servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Home on 9/16/2015.
+ */
+@WebServlet(name = "Blog")
+public class Blog extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/includes/navigation.jsp
@@ -0,0 +1,7 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<div class="col-md-5">
+    <ul>
+      <li><a href="/index"> Home </a></li>
+      <li><a href="/files"> Resources </a></li>
+    </ul>
+</div>
